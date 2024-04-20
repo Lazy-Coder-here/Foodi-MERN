8f04a6b929fd2c46f90acd33c44111939b9865c9
Rahul <rahulbsw2@gmail.com>
2024-04-20 22:04:47 +0530

    Added Home Page

--- food-client/src/App.jsx
+++ food-client/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className=' bg-green font-primary'>Hello, Rahul</h1>
+      <h1 className=" bg-green font-primary">Hello, Rahul</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- food-client/src/pages/home/Home.jsx
+++ food-client/src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Banner from "../../components/Banner";
+import Categories from "./Categories";
+import SpecialDishes from "./SpecialDishes";
+import Testimonials from "./Testimonials";
+import OurSevices from "./OurSevices";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Banner />
+      <Categories />
+      <SpecialDishes />
+      <Testimonials />
+      <OurSevices />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
